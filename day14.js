53a68d4c2cbb99c189b431318f65a897700b7464
Jim <jim.haz34@gmail.com>
2021-12-04 12:04:06 -0500

    day14 solution optimization

--- day14.js
+++ day14.js
@@ -1,20 +1,35 @@
+// const checkAllGauges = (gaugeList) => {
+//   // Code here!
+//   const gaugeListCheck = gaugeList.map((individualgauge)=> {
+//     if(individualgauge.current > individualgauge.min && individualgauge.current < individualgauge.max) {
+//       // console.log(individualgauge.current, individualgauge.min, individualgauge.max)
+//       return true
+//     } else {
+//       return false
+//     }
+//   })
+//   console.log(gaugeListCheck)
+//   let check = true
+//   for (let i = 0; i < gaugeListCheck.length; i++) {
+//     if(gaugeListCheck[i] === false) {
+//       check = false
+//     }
+//   }
+//   console.log(check)
+//   return check
+// }
+
+
+
 const checkAllGauges = (gaugeList) => {
   // Code here!
-  const gaugeListCheck = gaugeList.map((individualgauge)=> {
-    if(individualgauge.current > individualgauge.min && individualgauge.current < individualgauge.max) {
-      // console.log(individualgauge.current, individualgauge.min, individualgauge.max)
-      return true
+  // let check = true
+  for (let i = 0; i < gaugeList.length; i++) {
+    if(gaugeList[i].current > gaugeList[i].min && gaugeList[i].current < gaugeList[i].max) {
     } else {
+      // check = false
       return false
     }
-  })
-  console.log(gaugeListCheck)
-  let check = true
-  for (let i = 0; i < gaugeListCheck.length; i++) {
-    if(gaugeListCheck[i] === false) {
-      check = false
-    }
   }
-  console.log(check)
-  return check
+  return true
 }
