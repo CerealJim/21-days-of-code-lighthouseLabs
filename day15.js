fb3bc6aa357cec7ac7e4a6b6a6949f0654fc966c
Jim <jim.haz34@gmail.com>
2021-12-05 16:12:57 -0500

    day15 solution changes the value of the isOn property to true by mutating the original array that is passed into the function as an argument

---
+++ day15.js
@@ -0,0 +1,10 @@
+
+const switchAllTogglesOn = (toggleList) => {
+  // Code here!
+  toggleList.map((toggle)=>{
+    // console.log(toggle.isOn = true)
+    toggle.isOn = true
+  })
+  // console.log(toggleList)
+  return toggleList
+}
